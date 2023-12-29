58f884961fc23ab85c9f146fb9bae37599c58b72
Moosieus <cameron.duley99@gmail.com>
2023-12-29 14:58:22 -0500

    fix package files (`cargo.toml`)

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Bzip2.MixProject do
     [
       files: [
         "lib",
-        "native/bzip2_decomp/.cargo/config",
+        "native/bzip2_decomp/.cargo/config.toml",
         "native/bzip2_decomp/src",
         "native/bzip2_decomp/Cargo.toml",
         "native/bzip2_decomp/Cargo.lock",
